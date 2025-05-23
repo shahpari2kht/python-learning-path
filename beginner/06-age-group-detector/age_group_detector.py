0a3868c17ae4e361d72937f0c35a74e1b51979d1
Parisa Mohammadzadeh <110891110+shahpari2kht@users.noreply.github.com>
2025-05-23 19:54:31 +0330

    Create age_group_detector.py

---
+++ beginner/06-age-group-detector/age_group_detector.py
@@ -0,0 +1,13 @@
+age=int(input())
+if age>0 and age<6:
+    print("khordsal")
+elif age>=6 and age<10:
+    print("koodak")
+elif 10<=age<14:
+    print("nojavan")    
+elif 14<=age<24:
+    print("javan")
+elif 24<=age<40:
+    print("bozorgsal")
+elif age>=40:
+    print("miansal")
