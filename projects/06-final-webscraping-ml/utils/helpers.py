9635fbffa567df4bb8beac02735d99c74aa017f3
Parisa Mohammadzadeh <110891110+shahpari2kht@users.noreply.github.com>
2025-05-24 14:03:57 +0330

    Update helpers.py

--- projects/06-final-webscraping-ml/utils/helpers.py
+++ projects/06-final-webscraping-ml/utils/helpers.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import os
+
+def save_to_csv(data, path):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    df = pd.DataFrame(data)
+    df.to_csv(path, index=False)
+    print(f"داده در {path} ذخیره شد")
 
